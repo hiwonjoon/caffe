29a467d5c7ab5d59d4102c84caf4977a08c59ff3
Wonjoon Goo <hi.wonjoon@gmail.com>
2015-04-16 15:32:24 +0900

    runnig script featured added. - auto detect intermediate result - run as background

--- research/run.sh
+++ research/run.sh
@@ -5,5 +5,40 @@ then
 	exit 1
 fi
 cd $1
-../../build/tools/caffe train -solver ./solver.prototxt -weights ../../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -gpu 0
+
+list=($(ls -1 ./*.solverstate | tr '\n' '\0' | xargs -0 -n 1 basename | sort -V -r))
+filename=$(date +"%F_%H_%M")
+echo $filename
+
+if [ ${#list[@]} -ge 1 ]
+then
+	read -p "You have a solverstate. Do you want to continue learning process from the last(y/n)? " answer
+	case ${answer:0:1} in
+		y|Y )
+			../../build/tools/caffe train -solver ./solver.prototxt -gpu 0 -snapshot ./$list &> $filename.log &
+		;;
+		* )
+			../../build/tools/caffe train -solver ./solver.prototxt -weights ../../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -gpu 0 &> $filename.log &
+		;;
+	esac
+fi
+
 cd ..
+
+#script for future use
+
+#!/bin/bash
+#list=$(ls -1 ./regularized_fix/*.solverstate | tr '\n' '\0' | xargs -0 -n 1 basename)
+#for file in $list
+#do
+#	echo $file
+#done
+#files=./regularized_fix/"*.solverstate"
+#regex='([0-9]+)\.solverstate'
+#for f in $files
+#do
+#	[[ $f =~ $regex ]]
+#	echo ${BASH_REMATCH[1]}
+#done
+
+#list=$(ls -1 ./regularized_fix/*.solverstate | tr '\n' '\0' | xargs -0 -n 1 basename | sort -V)
